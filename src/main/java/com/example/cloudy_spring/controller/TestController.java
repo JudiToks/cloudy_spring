a8b07055e61c78f6c060f392e4ae7a5be6a567bf
Judicaël Rakotoarisedy <judi.rakotoarisedy@gmail.com>
2024-01-09 01:04:10 +0300

    lundi ariva

--- src/main/java/com/example/cloudy_spring/controller/TestController.java
+++ src/main/java/com/example/cloudy_spring/controller/TestController.java
@@ -1,16 +1,12 @@
 package com.example.cloudy_spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
-@RequestMapping("/test")
 public class TestController
 {
-    @GetMapping
+    @GetMapping({"/hello"})
     public String sayHello()
     {
         System.out.println("Hello");
