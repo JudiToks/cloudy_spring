7e7bd9de4956892c38b8c994bd5a24afe23d2f5c
Judicaël Rakotoarisedy <judi.rakotoarisedy@gmail.com>
2024-01-29 11:02:22 +0300

    deploy 3

--- src/main/java/com/example/cloudy_spring/models/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.cloudy_spring.models;
-
-public enum Role {
-
-    USER,
-    ADMIN
-}
--- src/main/java/com/example/cloudy_spring/repository/AnnonceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cloudy_spring.repository;
-
-import com.example.cloudy_spring.models.Annonce;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AnnonceRepository extends CrudRepository<Annonce, Integer>
-{
-    Annonce findById(int id_annonce);
-}
